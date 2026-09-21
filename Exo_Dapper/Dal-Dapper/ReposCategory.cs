3a46c9203106277ed32e2f7ccf9365387cdec0c5
mehdi ziane <m.ziane@stag.technofuturtic.education>
2026-09-21 11:17:06 +0200

    add showcategory n showproduct

---
+++ Exo_Dapper/Dal-Dapper/ReposCategory.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using Exo_Dapper.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Exo_Dapper.Dal_Dapper
+{
+    public class ReposCategory
+    {
+        string connectionString = @"Server=GOS-VDI410\TFTIC;Database=CatalogueDB;Trusted_connection=True;TrustServerCertificate=True";
+
+        public IEnumerable<Category> ShowCategory()
+        {
+            using SqlConnection conn = new SqlConnection(connectionString);
+
+            return conn.Query<Category>("select * from category");
+        }
+    }
+}
---
+++ Exo_Dapper/Dal-Dapper/ReposProduct.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using Exo_Dapper.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Exo_Dapper.Dal_Dapper
+{
+    public class ReposProduct
+    {
+        string connectionString = @"Server=GOS-VDI410\TFTIC;Database=CatalogueDB;Trusted_connection=True;TrustServerCertificate=True";
+
+        public IEnumerable<Product> ShowProduct()
+        {
+            using SqlConnection conn = new SqlConnection(connectionString);
+
+            return conn.Query<Product>("select * from product");
+        }
+    }
+}
