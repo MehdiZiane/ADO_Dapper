16bcfa7641d6483eed0e85385983d87ce1f4f39d
mehdi ziane <m.ziane@stag.technofuturtic.education>
2026-09-21 16:59:20 +0200

    add models create product and create category

---
+++ Exo_Dapper/Models/CreateCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exo_Dapper.Models
+{
+    internal class CreateCategory
+    {
+        public CreateCategory(string titre, string description)
+        {
+            this.category_titre = titre;
+            this.category_description = description;
+        }
+        public string category_titre { get; set; } 
+        public string category_description { get; set; }
+    }
+}
---
+++ Exo_Dapper/Models/CreateProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exo_Dapper.Models
+{
+    internal class CreateProduct
+    {
+        public CreateProduct(string titre, string description, int stock, int categoryId)
+        {
+            this.product_titre = titre;
+            this.product_description = description;
+            this.product_stock = stock;
+            this.category_id = categoryId;
+        }
+        public string product_titre { get; set; }
+        public string product_description { get; set; }
+        public int product_stock { get; set; }
+        public int category_id { get; set; }
+    }
+}
